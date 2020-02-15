d41a8e84fdf0ee2ef13a819ddb4230cd90888d22
Umberto Lentini <umberto.lentini@gmail.com>
2020-02-15 12:07:50 +0100

    Small typescript fix

--- src/use-reduced-state.tsx
+++ src/use-reduced-state.tsx
@@ -2,7 +2,7 @@ import { useReducer } from "react"
 
 export function useReducedState(initialState: any) {
   const reducedState = useReducer(
-    (oldState, newState) => ({ ...oldState, ...newState }),
+    (oldState: any, newState: any) => ({ ...oldState, ...newState }),
     initialState,
   )
   return reducedState
